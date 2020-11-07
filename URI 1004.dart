56123109e87e5a5dde41b1a8783544514f5a8617
Felipe de Luca <felipedelucanogueira@gmail.com>
2020-11-07 19:55:23 -0300

    Add files via upload

---
+++ URI 1004.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+
+// Leia dois valores inteiros. A seguir, calcule o produto entre estes dois valores e atribua esta operação à variável PROD. A seguir mostre a variável PROD com mensagem correspondente.
+
+// Entrada
+// O arquivo de entrada contém 2 valores inteiros.
+
+// Saída
+// Imprima a mensagem "PROD" e a variável PROD conforme exemplo abaixo, com um espaço em branco antes e depois da igualdade. Não esqueça de imprimir o fim de linha após o produto, caso contrário seu programa apresentará a mensagem: “Presentation Error”.
+
+void main() {
+  var atext = stdin.readLineSync();
+  int a = int.parse(atext);
+  var btext = stdin.readLineSync();
+  int b = int.parse(btext);
+
+  int prod = a * b;
+
+  print("PROD = $prod");
+}
