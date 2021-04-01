9b2a1b1f3c46f599bf59840be27e6f08d4215e54
Felipe de Luca <felipedelucanogueira@gmail.com>
2021-04-01 23:07:18 +0000

    Adicionada Pasta Apps

--- OO/classe_data.dart
+++ OO/classe_data.dart
@@ -56,3 +56,62 @@ main(){
   print(Data.ultimoDiaDoAno(1234));
 
 }
+
+
+
+
+
+
+
+
+#==================#
+
+import 'OO/modelo/venda.dart';
+import 'OO/modelo/cliente.dart';
+import 'OO/modelo/venda_item.dart';
+import 'OO/modelo/produto.dart';
+
+
+main() {
+  var venda = Venda(
+    cliente : Cliente(
+      nome: 'Felipe de Luca',
+      cpf: '123.456.654-23'
+
+    ),
+    itens:<VendaItem>[
+      VendaItem(
+      quantidade:10,
+        produto: Produto(
+          codigo: 1,
+          nome: 'Trigo ',
+          preco: 4.42,
+          desconto: 0.5
+        )
+      ),
+      VendaItem(
+        quantidade: 8,
+        produto:Produto(
+        codigo:123,
+        nome : 'Feijao',
+        preco: 19.99,
+        desconto: 0.2
+        )
+      ),
+      VendaItem(
+      quantidade: 100,
+      produto:Produto(
+      codigo:31,
+      nome:'Borracha',
+      preco: 10.9,
+      desconto: 0.3
+      )
+      )
+    ]
+  );
+
+print("O valor total da venda é: ${venda.valorTotal}");
+print("O nome do primeiro produto é: ${venda.itens[0].produto.nome}");
+
+
+}
--- main.dart
+++ main.dart
@@ -1,49 +1,19 @@
-import 'OO/modelo/venda.dart';
-import 'OO/modelo/cliente.dart';
-import 'OO/modelo/venda_item.dart';
-import 'OO/modelo/produto.dart';
-
-
 main() {
-  var venda = Venda(
-    cliente : Cliente(
-      nome: 'Felipe de Luca',
-      cpf: '123.456.654-23'
 
-    ),
-    itens:<VendaItem>[
-      VendaItem(
-      quantidade:10,
-        produto: Produto(
-          codigo: 1,
-          nome: 'Trigo ',
-          preco: 4.42,
-          desconto: 0.5
-        )
-      ),
-      VendaItem(
-        quantidade: 8,
-        produto:Produto(
-        codigo:123,
-        nome : 'Feijao',
-        preco: 19.99,
-        desconto: 0.2
-        )
-      ),
-      VendaItem(
-      quantidade: 100,
-      produto:Produto(
-      codigo:31,
-      nome:'Borracha',
-      preco: 10.9,
-      desconto: 0.3
-      )
-      )
-    ]
-  );
+calcularImc(30 , 1.90);
 
-print("O valor total da venda é: ${venda.valorTotal}");
-print("O nome do primeiro produto é: ${venda.itens[0].produto.nome}");
+}
 
+calcularImc(double peso , double altura) {
+
+  var imc = peso / (altura * altura);
+
+  if(imc <= 18.5){
+    print('Voce esta abaixo do peso');
+  }else if(){
+      print('Voce esta no peso ideal');
+  }
+
+  print('O seu IMC é : $imc');
 
 }
